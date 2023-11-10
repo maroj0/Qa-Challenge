f05c42eb643359946f8f06bd3232fe0b16772571
Martin Ojeda <maroj074@gmail.com>
2023-11-10 00:03:04 -0300

    feat: test form

---
+++ features/test_incomplete_with_product_buy_form.py
@@ -0,0 +1,25 @@
+from screenpy import Actor, given, then, when
+from screenpy.actions import See
+from screenpy.pacing import act, scene
+from screenpy_selenium.actions import Open
+from screenpy.resolutions import (
+    ContainsTheText,
+    IsEqualTo
+)
+from screens.login import LoginPage
+from screens.home_screen import HomePage
+from tasks.login_task import LogIn
+from screens.checkout_screen import CheckoutPage
+from tasks.incomplete_checkout_form_task import InCompleteCheckoutForm
+from tasks.add_product_task import AddProductTask
+
+from questions.SearchText import SearchText
+
+@act("Buy")
+@scene("Complete first form")
+def test_add_product(Dev: Actor) -> None:
+    given(Dev).was_able_to(Open.their_browser_on(LoginPage().URL), LogIn.log_in(), Open.their_browser_on(HomePage.URL),AddProductTask())
+    when(Dev).attempts_to(InCompleteCheckoutForm())
+    then(Dev).should(
+        See.the(SearchText(CheckoutPage.ERROR), ContainsTheText("Error")),
+    )
---
+++ tasks/incomplete_checkout_form_task.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.common.keys import Keys
+
+from screenpy import Actor
+from screenpy.pacing import beat
+from screenpy_selenium.actions import Enter, Wait, Click, Pause
+from screens.login import LoginPage
+from screens.checkout_screen import CheckoutPage
+from screens.cart_screen import CartPage
+from screens.home_screen import HomePage
+
+class InCompleteCheckoutForm:
+    
+
+    @staticmethod
+    def InCompleteCheckoutForm() -> "InCompleteCheckoutForm":
+        return InCompleteCheckoutForm()
+    
+    @beat('CompleteCheckoutForm')
+    def perform_as(self, the_actor:Actor) -> None:
+        the_actor.attempts_to(
+            Click.on(HomePage.CART),
+            Wait.for_the(CartPage.BUTTON_CHECKOUT),
+            Click.on(CartPage.BUTTON_CHECKOUT),
+            Wait.for_the(CheckoutPage.ZIP_INPUT),
+            # Enter.the_text("Dev").into(CheckoutPage.NAME_INPUT),
+            # Enter.the_text("Dev").into(CheckoutPage.LAST_NAME_INPUT),
+            # Enter.the_text("1234").into(CheckoutPage.ZIP_INPUT),
+            Click.on(CheckoutPage.CONTINUE_BUTTON),
+            # Wait.for_the(CheckoutPage.CHECK)
+            )
